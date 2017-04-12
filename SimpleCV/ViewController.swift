38c700728aa50ec07f8449fe41f54a280e7c70f3
Ed <edmccormic@gmail.com>
2017-04-12 08:58:58 -0600

    first commit

--- SimpleCV/ViewController.swift
+++ SimpleCV/ViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var suitsArray = [Dictionary<String, AnyObject>]()
+    
+    enum ParsingError: Error {
+        case MissingJson
+        case JsonParsingError
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +26,25 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK:
+    // MARK: Data setup
+    
+    
+    func setupData () throws {
+        
+        guard let filePath = Bundle.main.path(forResource: "cards", ofType: "json"), let jsonData = NSData(contentsOfFile: filePath) else {
+            throw ParsingError.MissingJson
+        }
+        
+        do {
+            let parsedObject = try JSONSerialization.jsonObject(with: jsonData as Data, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
+            suitsArray = parsedObject["suits"] as! Array
+        } catch {
+            throw ParsingError.JsonParsingError
+        }
+        
     }
 
 
